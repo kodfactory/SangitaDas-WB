5af8a7ae4bc49856972100690b01b6dd0868aacb
kodfactory <52972405+kodfactory@users.noreply.github.com>
2020-09-15 21:20:04 +0530

    Add files via upload

---
+++ IfStatement.py
@@ -0,0 +1,18 @@
+num = 2
+
+#if num > 5 and num < 20 :
+#    print("Number is between 5 and 20")
+#    print("Some other code in if statement")
+
+#print("Common code")
+
+if num > 10 :
+    print("Number is greater then 10")
+else:
+    print("Number is not greater then 10")
+
+# Homework: take any number and print whether
+# that number is odd or even
+
+
+    
---
+++ VariablesNaming.py
@@ -0,0 +1,37 @@
+# Rules to declare variables
+
+# 1: Variable name should not start with uppercase
+# 2: Variable name must not start with digit or contains special character (including space)
+
+#2name = 'Sangita'
+#name2 = 'Sangita'
+#Name = 'Sangita'
+
+#firstname
+#first name = 'Sangita'
+
+#print(name2)
+
+# Camlecase naming convention :
+# First character of every word will be upper case except 1st word
+
+#firstname = 'Sangita'
+#lastname = 'Das'
+
+#firstnameandlastname = 'Sangita Das' # Invalid or not good
+
+#print(firstnameandlastname)
+
+#firstNameAndLastName = 'Sangita Das' # Valid
+
+# first_name_and_last_name = 'Sangita Das' # Valid
+
+
+
+
+
+
+
+
+
+
