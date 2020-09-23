3b3fec4fe0a7610b0e09bd80a5644b34140b6475
kodfactory <52972405+kodfactory@users.noreply.github.com>
2020-09-23 10:57:45 +0530

    Add files via upload

---
+++ ForLoopExample.py
@@ -0,0 +1,55 @@
+# Loops: To repeat certain task for n number of times
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5, 25):
+#     print(i)
+
+# for i in range(1,51):
+#     if i % 2 == 0:
+#         print(i)
+
+# for i in range(1, 50, 3):
+#     print(i)
+
+# print odd numbers using step
+# To print even we can use `range(0,51,2)`
+
+# for i in range(1, 51, 2):
+#     print(i)
+
+# print('A \nB')
+
+# print('\\n is used to print the new line, and \\n is very useful')
+
+# print(r'\n is used to print the new line, and \n is very useful')
+
+# print('A', end='-')
+# print('B')
+
+# for i in "Sangita":
+#     print(i, end='')
+
+# Create a table by taking the user input
+# For Ex: 3
+
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# 3 x 4 = 12
+# 3 x 5 = 15
+# 3 x 6 = 18
+# 3 x 7 = 21
+# 3 x 8 = 24
+# 3 x 9 = 27
+# 3 x 10 = 30
+
+# Take the user input and display traingle of that value
+# For Ex: 5
+
+# *
+# **
+# ***
+# ****
+# *****
---
+++ WhileLoopExample.py
@@ -0,0 +1,14 @@
+# while <CONDITION>:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+i = 2
+
+while i <= 10:
+    print(i)
+    i += 1 # i = i + 1
+
+
+
